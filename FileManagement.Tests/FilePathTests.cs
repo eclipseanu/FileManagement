c7d6f64c9656f006ecb14489738e12a7649247e6
jason_storey <jasonstoreyireland@gmail.com>
2020-04-13 14:58:43 +0100

    Adds FilePath tests

---
+++ FileManagement.Tests/FilePathTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace FileManagement.Tests
+{
+    [TestFixture]
+    public class FilePathTests
+    {
+        private const string THIS_COMPUTER_PROJECT_RUNNING_LOCATION = @"C:\Users\Jason Storey\Desktop\GITHUB\FileManagement\FileManagement.Tests\bin\Debug";
+        private const string THIS_COMPUTER_PROJECT_MAIN_DIRECTORY = @"C:\Users\Jason Storey\Desktop\GITHUB\FileManagement";
+        private const string THIS_COMPUTER_RUNNING_DRIVE = @"C:";
+
+        [Test]
+        public void This_Computer_Running_Directory()
+        {
+            var here = FilePath.ApplicationRunningDirectory;
+            here.Should().Be(THIS_COMPUTER_PROJECT_RUNNING_LOCATION);
+        }
+
+        [Test]
+        public void Back_X_Directories()
+        {
+            var projectMainFolder = FilePath.BackXDirectories(3, FilePath.ApplicationRunningDirectory);
+            projectMainFolder.Should().Be(THIS_COMPUTER_PROJECT_MAIN_DIRECTORY);
+        }
+
+        [Test]
+        public void Back_Negative_Directories()
+        {
+            var newDir = FilePath.BackXDirectories(-50, THIS_COMPUTER_PROJECT_MAIN_DIRECTORY);
+            newDir.Should().Be(THIS_COMPUTER_PROJECT_MAIN_DIRECTORY);
+        }
+
+        [Test]
+        public void Back_Too_Many_Directories_Returns_Root()
+        {
+            var newDir = FilePath.BackXDirectories(100, THIS_COMPUTER_PROJECT_MAIN_DIRECTORY);
+            newDir.Should().Be(THIS_COMPUTER_RUNNING_DRIVE);
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ FileManagement/FilePath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FileManagement
+{
+    public class FilePath
+    {
+        public static string ApplicationRunningDirectory => AppDomain.CurrentDomain.BaseDirectory;
+
+        public static string BackXDirectories(int amount, string path)
+        {
+            if (amount <= 0) return path;
+            var backup = string.Concat(Enumerable.Repeat(@"..\",amount));
+            return Path.GetFullPath(Path.Combine(path, backup)).TrimEnd('\\');
+        }
+    }
+}
